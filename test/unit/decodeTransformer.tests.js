5bd666e5bd169e09b2fbb4635dc601f38a13ab70
Damian Stanger <damian.stanger@rbi.co.uk>
2018-08-02 21:50:37 +0100

    added tests for the decodeTransformer

---
+++ test/unit/decodeTransformer.tests.js
@@ -0,0 +1,72 @@
+/* eslint-disable no-undefined */
+const chai = require("chai");
+const expect = chai.expect;
+chai.should();
+
+const Readable = require("stream").Readable;
+
+const messageBuilder = require("../../src/source/messageBuilder");
+const getDecodeTransformer = require("../../src/decodeTransformer");
+
+
+function getReadStreamWithMessaage(message) {
+  return new Readable({
+    "objectMode": true,
+    "highWaterMark": 1,
+    read() {
+      this.push(message);
+      this.push(null);
+    }
+  });
+}
+
+
+describe("decodeTransformer", () => {
+  it("should do nothing if there is no payload", () => {
+    const originalMessage = messageBuilder().withPayload(undefined).build();
+    const readStream = getReadStreamWithMessaage(originalMessage);
+
+    const decodeTransformer = getDecodeTransformer();
+    decodeTransformer.on("data", message => {
+      expect(message.payload).to.be.undefined;
+    });
+
+    readStream.pipe(decodeTransformer);
+  });
+
+  it("should do nothing if the payload is an object", () => {
+    const originalMessage = messageBuilder().withPayload({"foo": "bar"}).build();
+    const readStream = getReadStreamWithMessaage(originalMessage);
+
+    const decodeTransformer = getDecodeTransformer();
+    decodeTransformer.on("data", message => {
+      message.payload.should.deep.equal({"foo": "bar"});
+    });
+
+    readStream.pipe(decodeTransformer);
+  });
+
+  it("should do nothing if the payload is a none base64 encoded string", () => {
+    const originalMessage = messageBuilder().withPayload("FooBar").build();
+    const readStream = getReadStreamWithMessaage(originalMessage);
+
+    const decodeTransformer = getDecodeTransformer();
+    decodeTransformer.on("data", message => {
+      message.payload.should.equal("FooBar");
+    });
+
+    readStream.pipe(decodeTransformer);
+  });
+
+  it("should decode the payload to an object from a base 64 encoded string", () => {
+    const originalMessage = messageBuilder().withPayload("eyJmb28iOiAiYmFyIiwgIm9iaiI6IHsiZml6eiI6IDEyMzQuNTY3OH19").build();
+    const readStream = getReadStreamWithMessaage(originalMessage);
+
+    const decodeTransformer = getDecodeTransformer();
+    decodeTransformer.on("data", message => {
+      message.payload.should.deep.equal({"foo": "bar", "obj": {"fizz": 1234.5678}});
+    });
+
+    readStream.pipe(decodeTransformer);
+  });
+});
