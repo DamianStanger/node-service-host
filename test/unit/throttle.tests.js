efc911d9d1ca7e15879f3be93150b6ecdf59d341
Damian Stanger <damian.stanger@rbi.co.uk>
2018-08-03 09:09:07 +0100

    finished off the throttle tests

--- test/unit/throttle.tests.js
+++ test/unit/throttle.tests.js
@@ -8,8 +8,8 @@ const messageBuilder = require("../../src/source/messageBuilder");
 
 
 const standardConfig = {
-  "millisecondsToWaitOnNoMessages": 20,
-  "millisecondsToWaitOnError": 10,
+  "millisecondsToWaitOnNoMessages": 0,
+  "millisecondsToWaitOnError": 0,
   "maxProcessingConcurrency": 2
 };
 
@@ -73,7 +73,7 @@ describe("throttle", () => {
     resolvedMessages[1].should.equal(message2);
   });
 
-  it("should resume the read stream when processing a message completes", () => {
+  it("should resume the read stream when a message completes its processing", () => {
     const message1 = messageBuilder().build();
     const message2 = messageBuilder().build();
     const message3 = messageBuilder().build();
@@ -97,6 +97,68 @@ describe("throttle", () => {
         resolvedMessages[2].should.equal(message3);
         resolve();
       }, 5);
+    });
+  });
+
+  describe("When a control message is received", () => {
+    describe("Which indicates no messages to process", () => {
+      it("should pause the read stream for 10 milliseconds", () => {
+        const controlMessage = messageBuilder().buildControlMessage();
+        const message = messageBuilder().build();
+        const readStream = getReadStreamWithMessaages([controlMessage, message]);
+        const messageDelegator = getMessageDelegator();
+
+        const customConfig = Object.assign({}, standardConfig, {"millisecondsToWaitOnNoMessages": 10});
+        throttle(readStream, messageDelegator, customConfig);
+        readStream.read();
+
+        readStream.isPaused().should.be.true;
+        resolvedMessages.length.should.equal(0);
+
+        return new Promise(resolve => {
+          setTimeout(() => {
+            readStream.isPaused().should.be.true;
+            resolvedMessages.length.should.equal(0);
+          }, 6);
+
+          setTimeout(() => {
+            readStream.isPaused().should.be.false;
+            resolvedMessages.length.should.equal(1);
+            resolvedMessages[0].should.equal(message);
+            resolve();
+          }, 14);
+        });
+      });
+    });
+
+    describe("Which indicates an error occurred", () => {
+      it("should pause the read stream for 10 milliseconds", () => {
+        const controlMessage = messageBuilder().withPayload({"error": new Error("foobar")}).buildControlMessage();
+        const message = messageBuilder().build();
+        const readStream = getReadStreamWithMessaages([controlMessage, message]);
+        const messageDelegator = getMessageDelegator();
+
+        const customConfig = Object.assign({}, standardConfig, {"millisecondsToWaitOnError": 10});
+        throttle(readStream, messageDelegator, customConfig);
+        readStream.read();
+
+        readStream.isPaused().should.be.true;
+        resolvedMessages.length.should.equal(0);
+
+        return new Promise(resolve => {
+          setTimeout(() => {
+            readStream.isPaused().should.be.true;
+            resolvedMessages.length.should.equal(0);
+          }, 6);
+
+          setTimeout(() => {
+            readStream.isPaused().should.be.false;
+            resolvedMessages.length.should.equal(1);
+            resolvedMessages[0].should.equal(message);
+            resolve();
+          }, 14);
+        });
+      });
     });
   });
 });
