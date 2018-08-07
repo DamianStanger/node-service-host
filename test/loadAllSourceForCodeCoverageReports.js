96cfd6b41656c1679e0528a30126ca8eb1814bdd
Damian Stanger <damian.stanger@rbi.co.uk>
2018-08-07 07:44:13 +0100

    added test that can load all the source files to ensure all js is included in the test report regardless of if a test touches it

---
+++ test/loadAllSourceForCodeCoverageReports.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-sync */
+/* eslint-disable no-console */
+
+const fs = require("fs");
+const path = require("path");
+
+
+function isDirectory(fileName) {
+  const stats = fs.lstatSync(fileName);
+  return stats.isDirectory();
+}
+
+function requireAndSwallowErrors(fileName) {
+  try {
+    // console.log(`require(${fileName})`);
+    require(fileName);
+  } catch (err) {
+    console.log("[WARN]", `caught error running require(${fileName})`);
+  }
+}
+
+function requireAllTheThings(dirName, exclude) {
+  fs.readdir(dirName, (err, files) => {
+    if (err) {
+      throw new Error(err);
+    }
+
+    files.map(fileName => path.join(dirName, fileName))
+      .filter(isDirectory)
+      .forEach(requireAllTheThings);
+
+    files.filter(fileName => fileName.endsWith(".js"))
+      .map(fileName => path.join("../", dirName, fileName))
+      .filter(fileName => fileName !== exclude)
+      .forEach(requireAndSwallowErrors);
+  });
+}
+
+
+describe("Load all source files for code coverage reports", () => {
+  it("require", done => {
+    const exclude = "../src/source/testSource.js";
+    requireAllTheThings("./src", exclude);
+    done();
+  });
+});
